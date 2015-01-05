8e228e26370d60163a8a4e134867107e088d29b6
Sean Hickey <shickey.dev@gmail.com>
2015-01-04 23:58:20 -0600

    Added scripts directory and ruby script for creating a new post

---
+++ scripts/new_post.rb
@@ -0,0 +1,38 @@
+require 'yaml'
+
+author_yaml = YAML.load_file('../_data/authors.yml')
+authors = author_yaml.keys
+
+print "What is your short name? (i.e., first name, all lowercase): "
+shortname = gets.chomp
+
+if not authors.include? shortname
+  puts "That's not a name I recognize!"
+else
+print "What is the title of your new post? "
+title = gets.chomp
+
+now = DateTime.now
+
+new_post = <<EOF
+---
+layout: post
+title: "#{title}"
+date: #{now.strftime '%Y-%m-%d %H:%M:%S'}
+author: #{shortname}
+---
+
+This is a brand new post!
+EOF
+
+friendly_title = title.downcase.gsub(/[^a-z ]/, '').gsub(' ', '-')
+
+filepath_relative_to_siteroot = "/#{shortname}/_posts/#{now.to_date.to_s}-#{friendly_title}.md"
+
+filepath = File.expand_path("../#{filepath_relative_to_siteroot}")
+
+File.open(filepath, 'w') { |file| file.write new_post }
+
+puts "Created new post at #{filepath_relative_to_siteroot}"
+
+end
